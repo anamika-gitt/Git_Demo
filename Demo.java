9c422f51818561daf311746f0e3229da4ef351cd
anamika-gitt <bhuniaanamika2002@gmail.com>
2025-09-04 20:49:18 +0530

    add java file

---
+++ Demo.java
@@ -0,0 +1,10 @@
+package Git_Demo;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+
+        System.out.println("This is a trial code");
+        System.out.println("First Repo");
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Git_Demo
-This is my first Git Repository.
+This is my first Git Repository !
 <br>
-Author - Anamika Bhunia.
+Author - Anamika Bhunia (student)
